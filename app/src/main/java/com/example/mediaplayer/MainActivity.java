f46e2df5045f4e1daa8a0b083e60116e8f12d880
1211186431 <56494120+1211186431@users.noreply.github.com>
2020-10-02 11:02:09 +0800

    10.2 上午 初始问题，歌词导入

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -14,9 +14,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,20 +29,26 @@ import com.example.mediaplayer.songsdb.SongsDB;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Random;
+import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "myTag";
     private MediaPlayer mediaPlayer;
     String music_name = "Carly Rae Jepsen - I Really Like You";
     String music_state = "正在播放";
-    String song_id="";                                    //初始可以随便找一个放，没有放really like you
-    String sheet_id="CC23FB90C65648D888FCC47358898A26";
+    Stack<String> allsong;
+    String song_id = "";                                    //初始可以随便找一个放，没有放really like you
+    String sheet_id = "";
     String path = "/storage/emulated/0/music_2/Good Time - Owl City,Carly Rae Jepsen.mp3";
     SeekBar seekBar;
     int istouch = 1;
+    String i1[]={"列表"};
+    TextView song_name;
     ArrayList<Map<String, String>> item1;
-    ArrayList<Map<String, String>> item2;
+
     //处理进度条更新
     Handler mHandler = new Handler() {
         @Override
@@ -51,25 +60,27 @@ public class MainActivity extends AppCompatActivity {
 
                     int time = mediaPlayer.getDuration();
                     int max = seekBar.getMax();
+                    double n = ((double) position) / 1000;
+                    double t1 = (double) time / 1000;
                     if (istouch == 1) {
 
                         seekBar.setProgress(position * max / time);
 
-                        double n = ((double) position) / 1000;
+
                         String n2 = String.format("%.2f", n);
                         String time2 = String.format("%.2f", ((double) time) / 1000);
                         TextView t = (TextView) findViewById(R.id.time2);
                         t.setText(n2 + "s / " + time2 + "s");
                     }
-                    if (!mediaPlayer.isPlaying()) {
-                        if (position >= time) {
-                            Log.v("Tag", "over");
-                            changeSong();     //顺序 执行下一首
-                                             //随机  执行随机下一首
-                                             //单曲Loop
-                        }
-
+                    // if (!mediaPlayer.isPlaying()) {
+                    if (Math.abs(t1 - n) < 0.1) {
+                        Log.v("Tag", "over");
+                        changeSong();     //顺序 执行下一首
+                        //随机  执行随机下一首
+                        //单曲Loop
                     }
+
+                    // }
                     break;
                 default:
                     break;
@@ -82,60 +93,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spring_Loop);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                String[] autoInfo = getResources().getStringArray(R.array.Loop2);
+                i1[0] = autoInfo[pos];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callbac
+            }
+        });
+        song_name =findViewById(R.id.song_name);
         final Intent intent = new Intent(MainActivity.this, PlayService.class);
         intent.putExtra("music_name", music_name);
         intent.putExtra("music_state", music_state);
-        startService(intent);
+        //startService(intent);
 
-
-        Button buttonLoop=findViewById(R.id.buttonLoop);
-        //循环播放
-        buttonLoop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v(TAG, "Looping");
-                boolean loop = mediaPlayer.isLooping();
-                mediaPlayer.setLooping(!loop);
-//                if (!loop)
-//                    txtLoopState.setText("循环播放");
-//                else
-//                    txtLoopState.setText("一次播放");
-            }
-        });
         final Button buttonup = (Button) findViewById(R.id.buttonup);
         final Button buttonPause = (Button) findViewById(R.id.buttonPause);
         final Button buttondown = (Button) findViewById(R.id.buttondown);
 
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         mediaPlayer = new MediaPlayer();
+
+        //初始化
         init();
 
         //上一首
         buttonup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Map<String,String> m=getUp(song_id);
-                String path2="";
-                if(m.isEmpty()) {
-                    path2="null";
-                }
-                else
-                    path2=m.get("path").toString();
-                Log.v("myTag",path2);
-                if(!path2.equals("null")){
-                    mediaPlayer.stop();
-                    try {
-                        mediaPlayer.reset();
-                        mediaPlayer.setDataSource(path2);
-                        mediaPlayer.prepare();
-                        mediaPlayer.start();
-                        song_id=m.get("_id").toString();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                else
-                    Toast.makeText(MainActivity.this,"error",Toast.LENGTH_LONG).show();
+                getUp();
             }
         });
 
@@ -143,28 +135,7 @@ public class MainActivity extends AppCompatActivity {
         buttondown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Map<String,String> m=getDown(song_id);
-                String path2="";
-                if(m.isEmpty()) {
-                    path2="null";
-                }
-                else
-                    path2=m.get("path").toString();
-                Log.v("myTag",path2);
-                if(!path2.equals("null")){
-                    mediaPlayer.stop();
-                    try {
-                        mediaPlayer.reset();
-                        mediaPlayer.setDataSource(path2);
-                        mediaPlayer.prepare();
-                        mediaPlayer.start();
-                        song_id=m.get("_id").toString();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-               }
-                else
-                    Toast.makeText(MainActivity.this,"error",Toast.LENGTH_LONG).show();
+                changeSong();
             }
         });
 
@@ -219,45 +190,48 @@ public class MainActivity extends AppCompatActivity {
         });
         init();
 
-        Button btn=findViewById(R.id.test);
+        Button btn = findViewById(R.id.test);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2=new Intent(MainActivity.this,sheet.class);
-                startActivityForResult(intent2,0);
+                Intent intent2 = new Intent(MainActivity.this, sheet.class);
+                startActivityForResult(intent2, 0);
             }
         });
 
 
-
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {  //传数据的测试
         super.onActivityResult(requestCode, resultCode, data);
-        SongsDB songsdb=new SongsDB(MainActivity.this);
-        Log.v("Tag",requestCode+" "+resultCode);
-        if(requestCode==0 && resultCode==1){
-            song_id=data.getStringExtra("song_id");
-            sheet_id=data.getStringExtra("sheet_id");
-            Songs.SongDescription song=songsdb.getSingleSong(song_id);
+        SongsDB songsdb = new SongsDB(MainActivity.this);
+        if (requestCode == 0 && resultCode == 1) {
+            song_id = data.getStringExtra("song_id");
+            sheet_id = data.getStringExtra("sheet_id");
+            Songs.SongDescription song = songsdb.getSingleSong(song_id);
             mediaPlayer.stop();
             try {
                 mediaPlayer.reset();
                 mediaPlayer.setDataSource(song.getPath());
                 mediaPlayer.prepare();
                 mediaPlayer.start();
-                song_id=song.getId();
+                song_id = song.getId();
+                allsong.push(song_id);
+                song_name.setText(song.getName());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void init() {
+    public void init() {  //初始化
         //进入Idle
         try {
             mediaPlayer.reset();
             mediaPlayer.setDataSource(path);
+            allsong = new Stack<String>();
+            allsong.push(path);           //初始要改
             mediaPlayer.prepare();
             mediaPlayer.start();
         } catch (IOException e) {
@@ -289,53 +263,161 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    public void changeSong(){   //结束时换歌
-        try {
-            mediaPlayer.reset();
-            mediaPlayer.setDataSource("/storage/emulated/0/Topic+09.mp3");
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    public void changeSong() {   //结束时换歌
+        switch (i1[0]){
+            case "列表":
+                Loop();
+                break;
+            case "随机":
+                random();
+                break;
+            case "单曲":
+                one_Loop();
+                break;
+            default:
+                Toast.makeText(MainActivity.this,"播放顺序错误",Toast.LENGTH_LONG).show();
+                getDown();
+                break;
         }
+
     }
-    public Map<String, String> getUp(String _id) { // 上一首
-        Map<String, String> b =new HashMap<>();
-        SongsDB songsDB=new SongsDB(this);
-        item1=songsDB.getAllSongs(sheet_id);
-        for(int i=0;i<this.item1.size();i++) {
-            if(item1.get(i).get("_id").equals(_id)) {
-                if(i-1>=0) {
-                    b=item1.get(i-1);
-                    break;
-                }
-                if(i==0){
-                    b=item1.get(item1.size()-1);
-                    break;
-                }
+
+    public Songs.SongDescription getUp(String _id) { // 上一首
+       // Map<String, String> b = new HashMap<>();
+        SongsDB songsDB = new SongsDB(this);
+        Songs.SongDescription s = null;
+        if (!allsong.isEmpty()) {
+            allsong.pop();
+            if (!(allsong.isEmpty())) {
+                Log.v("Stack",allsong.toString());
+                s=songsDB.getSingleSong(allsong.peek());
             }
+
         }
-        return b;
+
+        return s;
     }
+
+    public void getUp() {
+        Songs.SongDescription s=getUp(song_id);
+        String path2 = "";
+        if(s==null){
+            path2 = "null";
+        }
+        else
+            path2 = s.getPath();
+
+        Log.v("path2",path2);
+        if (!path2.equals("null")) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(path2);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                song_id = s.getId();
+                song_name.setText(s.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else
+            Toast.makeText(MainActivity.this, "没有上一首了", Toast.LENGTH_LONG).show();
+    }
+
 
     public Map<String, String> getDown(String _id) { // 下一首
-        Map<String, String> b =new HashMap<>();
-        SongsDB songsDB=new SongsDB(this);
-        item1=songsDB.getAllSongs(sheet_id);
-        Log.v("myTag",sheet_id+"  "+_id);
-        for(int i=0;i<this.item1.size();i++) {
-            if(item1.get(i).get("_id").equals(_id)) {
-                if(i+1<item1.size()) {
-                    b=item1.get(i+1);
-                    break;
-                }
-                if(i==item1.size()-1){
-                    b=item1.get(0);
-                    break;
+        Map<String, String> b = new HashMap<>();
+        SongsDB songsDB = new SongsDB(this);
+        if(!sheet_id.equals("")){
+            item1 = songsDB.getAllSongs(sheet_id);
+            for (int i = 0; i < this.item1.size(); i++) {
+                if (item1.get(i).get("_id").equals(_id)) {
+                    if (i + 1 < item1.size()) {
+                        b = item1.get(i + 1);
+                        break;
+                    }
+                    if (i == item1.size() - 1) {
+                        b = item1.get(0);
+                        break;
+                    }
                 }
             }
         }
+
         return b;
     }
 
+    public Map<String, String> getRandom(String _id) { //随机
+        Map<String, String> b = new HashMap<>();
+        SongsDB songsDB = new SongsDB(this);
+        if(!sheet_id.equals("")){
+            item1 = songsDB.getAllSongs(sheet_id);
+            Random r = new Random();
+            int ran1 = r.nextInt(item1.size() - 1);
+            b = item1.get(ran1);
+        }
+
+        return b;
+    }
+
+    public void getDown() {
+        Map<String, String> m = getDown(song_id);
+        String path2 = "";
+        if (m.isEmpty()) {
+            path2 = "null";
+        } else
+            path2 = m.get("path").toString();
+        if (!path2.equals("null")) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(path2);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                song_id = m.get("_id").toString();
+                allsong.push(song_id);
+                song_name.setText(m.get("name"));
+                Log.v("Stack","----"+allsong.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else
+            Toast.makeText(MainActivity.this, "没有下一首了", Toast.LENGTH_LONG).show();
+    }
+
+    public void one_Loop() { // 单曲循环
+        boolean loop = mediaPlayer.isLooping();
+        mediaPlayer.setLooping(true);
+    }
+
+    public void Loop() { // 顺序循环
+        mediaPlayer.setLooping(false);
+        getDown();
+    }
+
+    public void random() { // 随机
+        Map<String, String> m = getRandom(song_id);
+        String path2 = "";
+        if (m.isEmpty()) {
+            path2 = "null";
+        } else
+            path2 = m.get("path").toString();
+        if (!path2.equals("null")) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(path2);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+                song_id = m.get("_id").toString();
+                allsong.push(song_id);
+                song_name.setText(m.get("name"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else
+            Toast.makeText(MainActivity.this, "error", Toast.LENGTH_LONG).show();
+
+    }
 }
